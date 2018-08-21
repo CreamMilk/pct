a0f2f313407c5f28b3a4d33e972128e2162597b0
weibo6631352 <838524022@qq.com>
2018-08-21 16:59:34 +0800

    1，漏提交的

---
+++ PointCloudTool/mydef.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define ground_str "ground"
+#define veget_str "veget"
+#define budding_str "budding"
+#define power_line_str "power_line"
+#define tower_str "tower"
+#define unselect_str "unselect"
